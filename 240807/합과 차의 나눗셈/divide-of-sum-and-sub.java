0022f692853791a062c26fdafa43091ae982d54f
manNomi <98882987+manNomi@users.noreply.github.com>
2024-08-07 02:39:54 +0900

    Title: 합과 차의 나눗셈, Time: 103ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240807/합과 차의 나눗셈/divide-of-sum-and-sub.java
+++ 240807/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -8,11 +8,11 @@ public class Main {
         String[] input = br.readLine().split(" ");
         int num1 = Integer.parseInt(input[0]);
         int num2 = Integer.parseInt(input[1]);
-        double sum = num1+num2;
-        double min = num1-num2;
+        double sum = num1 + num2;
+        double min = num1 - num2;
         double result = sum / min;
         result=Math.round(result*100) / 100.0;
-        bw.write(String.format("%.2f", result));
+        bw.write(Double.toString(result));
         bw.newLine();
         bw.flush();
         bw.close();
