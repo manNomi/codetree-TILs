86ac0484c981b8b5c6bb55f98fa2a05d5f607c82
한만욱 <98882987+manNomi@users.noreply.github.com>
2026-05-13 21:45:53 +0900

    [Passed] 입력받는 수 부터 100까지 출력 (8ms, 0MB)

---
+++ trail1/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n ; 
+    cin >> n ; 
+
+    for (int i=n ; i <=100 ; i++){
+        cout << n << " "; 
+        n++;
+    }
+    return 0;
+}
